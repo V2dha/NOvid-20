8ea81333b36e7200c695cc613e7786568cda0721
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-02-06 20:02:42 +0530

    Algorithms with more accuracy

--- server/model.py
+++ server/model.py
@@ -1,214 +1,87 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
+#In[1]
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#In[2]
+path="Covid-Dataset.csv"
+df = pd.read_csv(path)
+sample=pd.DataFrame(df)
+
+#In[3]
+
+sample.Breathing_Problem.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Fever.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Dry_Cough.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Sore_throat.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Running_Nose.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Asthma.replace(('Yes', 'No'), (1, 0), inplace=True)
+sample.Chronic_Lung_Disease.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Headache.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Heart_Disease.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Diabetes.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Hyper_Tension.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Fatigue.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Gastrointestinal.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Abroad_travel.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Contact_with_COVID_Patient.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Attended_Large_Gathering.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Visited_Public_Exposed_Places.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Family_working_in_Public_Exposed_Places.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Wearing_Masks.replace(('Yes', 'No'), (1, 0), inplace=True) 
+sample.Sanitization_from_Market.replace(('Yes', 'No'), (1, 0), inplace=True)  
+sample.COVID.replace(('Yes', 'No'), (1, 0), inplace=True)  
 
 
-# In[2]:
-
-
-df = pd.read_csv('Cleaned-Data.csv')
-df.tail()
-
-
-# In[3]:
-
-
-
-
-
-# In[4]:
-
-
-df.shape
-
-
-# In[5]:
-
-
-
-
-
-# In[6]:
-
-
-df.dtypes
-
-
-# In[7]:
-
-
-df['Severity'] = (df.iloc[:, 19:23] == 1).idxmax(1)
-
-
-# In[8]:
-
-
+#In[4]
 df.head()
 
 
-# In[9]:
 
+#In[5]
+X=df[['Breathing_Problem','Fever','Dry_Cough','Sore_throat','Running_Nose','Asthma','Chronic_Lung_Disease',
+'Headache','Heart_Disease', 'Diabetes','Hyper_Tension','Fatigue','Gastrointestinal','Abroad_travel',
+'Contact_with_COVID_Patient','Attended_Large_Gathering','Visited_Public_Exposed_Places',
+'Family_working_in_Public_Exposed_Places','Wearing_Masks','Sanitization_from_Market']]
 
-df['Severity'].value_counts()
 
-
-# In[10]:
-
-
-
-
-# In[54]:
-
-
-df['Target'] = df['Severity'].replace('Severity_None', 0).replace('Severity_Mild', 1).replace('Severity_Moderate', 1).replace('Severity_Severe', 1)
-
-
-# In[12]:
-
-
-df.tail()
-
-
-# In[13]:
-
-
-df.iloc[:, 23:26].head()
-
-
-# In[14]:
-
-
-df['Contact'] = (df.iloc[:, 23:26] == 1).idxmax(1)
-
-
-# In[15]:
-
-
-df.head()
-
-
-# In[16]:
-
-
-df['Contact'].value_counts()
-
-
-# In[17]:
-
-
-df['Contact'] = df['Contact'].replace('Contact_No', 0).replace('Contact_Yes', 1).replace('Contact_Dont-Know', 2)
-
-
-# In[18]:
-
-
-df.head()
-
-
-# In[19]:
-
-
-df.dtypes
-
-
-# In[20]:
-
-
-df.iloc[:, 11:16].head()
-
-
-# In[21]:
-
-
-df['Age_Label'] = (df.iloc[:, 11:16] == 1).idxmax(1)
-df['Age_Label'] = df['Age_Label'].replace('Age_0-9', 0).replace('Age_10-19', 1).replace('Age_20-24', 2).replace('Age_25-59', 3).replace('Age_60+', 4)
-
-
-# In[22]:
-
-
-df.tail()
-
-
-# In[23]:
-
-
-df.iloc[:, 16:19].head()
-
-
-# In[24]:
-
-
-df['Gender'] = (df.iloc[:, 16:19] == 1).idxmax(1)
-df['Gender'] = df['Gender'].replace('Gender_Female', 1).replace('Gender_Male', 0).replace('Gender_Transgender', 2)
-
-
-# In[25]:
-
-
-df.head()
-
-
-# In[26]:
-
-
-df.columns
-
-
-# In[55]:
-
-
-X = df[['Fever', 'Tiredness', 'Dry-Cough', 'Difficulty-in-Breathing',
-       'Sore-Throat', 'Pains', 'Nasal-Congestion',
-       'Runny-Nose', 'Diarrhea', 'Contact', 'Age_Label',
-       'Gender']]
+#In[6]
 X.head(30)
 
 
-# In[56]:
+
+#In[7]
+Y=df[['COVID']]
 
 
-Y = df[['Target']]
+#In[8]
 Y.head()
 
 
-# In[57]:
-
-
+#In[9]
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 4)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2,random_state=2)
 x_train.shape, y_train.shape
 
 
-# In[58]:
+#In[10]
+from sklearn.ensemble import RandomForestClassifier
+RF = RandomForestClassifier()
+RF.fit(x_train, y_train)
+y_pred = RF.predict(x_test)
 
 
-from sklearn.linear_model import LogisticRegression
-LR = LogisticRegression()
-LR.fit(x_train, y_train)
-
-
-# In[59]:
-
-
-y_pred = LR.predict(x_test)
-
-
-# In[60]:
-
-
+#In[11]
 import sklearn.metrics as skm
 skm.multilabel_confusion_matrix(y_test, y_pred)
-
-
-# In[61]:
-
-
 print(skm.classification_report(y_test, y_pred))
+
+
+#In[12]
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
