0e9da796c7fcbd339634b9596a6ea35865510db1
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-02-18 11:16:59 +0530

    Updated app

--- server/app.py
+++ server/app.py
@@ -1,49 +1,49 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import pickle
+from flask.views import MethodView
 
-app = Flask(__name__, static_folder="static")
-model = pickle.load(open('ml.pkl', 'rb'))
+app = Flask(__name__,template_folder='template')
+model = pickle.load(open('model.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+def out():
+    return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST','GET'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    if request.method == 'POST':
+        int_features = [int(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = model.predict(final_features)
 
-    result = prediction[0]
+        result = prediction[0]
 
-    if (result == 1):
-        output = 'You have severe symptoms, you should consult a doctor.'
+        if result == 1:
+            output = 'You have severe symptoms, you should consult a doctor.'
 
-    # elif(result == 2):
-    #      output = 'You have moderate symptoms you may consult the doctor and take required precautions' 
+        else:
+            output = 'You have no symptoms. Keep taking precautionary measures.'
+        #return redirect(url_for('success', name=user))
+        return render_template('index.html', prediction_text='{}'.format(output))
 
-    # elif(result == 1):
-    #      output = 'You have mild symptoms you should take required precautions'   
-    
-    else:
-        output = 'You have no symptoms. Keep taking precautionary measures.'
-
-    return render_template('home.html', prediction_text='{}'.format(output))
-
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST','GET'] )
 def predict_api():
     '''
     For direct API calls trought request
     '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+        prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
+        output = prediction[0]
+        return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
