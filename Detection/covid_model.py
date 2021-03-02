1685434d5d5e3a2995651e67d846879a18d9c674
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-03-02 18:56:07 +0530

    Model for detection using X-Rays

---
+++ Detection/covid_model.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+from numpy import loadtxt
+import os
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Dropout, MaxPooling2D, Conv2D, Flatten, Input
+from tensorflow.keras.applications import VGG16
+from sklearn.metrics import classification_report, confusion_matrix
+from tensorflow.keras.models import load_model
+import pickle
+from PIL import Image
+from tensorflow.keras.models import load_model
+
+dir="Dataset"
+
+for dirname, _, filenames in os.walk(dir):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+train_data = tf.keras.preprocessing.image_dataset_from_directory(
+    'Dataset',
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(244, 244),
+    shuffle=True,
+    seed=123,
+    validation_split=0.2,
+    subset="training",
+    interpolation="bilinear",
+    follow_links=False,
+)
+test_data = tf.keras.preprocessing.image_dataset_from_directory(
+    'Dataset',
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(244, 244),
+    shuffle=True,
+    seed=123,
+    validation_split=0.2,
+    subset="validation",
+    interpolation="bilinear",
+    follow_links=False,
+)
+
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(3, activation='sigmoid'))
+
+model.compile(loss='sparse_categorical_crossentropy',optimizer="adam", metrics=['accuracy'])
+model.fit(train_data,validation_data=test_data,epochs = 20)
+model.save("model.h5")
+#model1.load_model('model.h5')
+print("Saved model to disk")
